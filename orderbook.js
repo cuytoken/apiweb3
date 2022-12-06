fbf74a12f037e65b319951a0afbd19ab2f0351f5
lenintarrillo <lenin.tarrillo.v@gmail.com>
2022-12-06 17:47:21 -0500

    UPDATE SCRIPT METAMASK

--- orderbook.js
+++ orderbook.js
@@ -4,45 +4,56 @@
 async function OrderBook_ApproveUSDC() {
 
     try {
+
+        var spender = ORDER_BOOK_ADDRESS; //Authorizes the amount of USDC to Spend for the SWAP contract
+        var amount = $("#amountApprove").val(); //6 decimals
+
         var contract = new web3.eth.Contract(USDC_TOKEN_ABI, USDC_TOKEN_ADRRESS);
-        spender = ORDER_BOOK_ADDRESS; //Authorizes the amount of USDC to Spend for the SWAP contract
-        amount= $("#amountApprove").val(); //6 decimals
-  
+        var txInstancia = contract.methods.approve(spender, amount); /*input: methodo  */
+        var encodedABI = txInstancia.encodeABI();
         var tx = {
-          from: account,
-          gasLimit: 10000000
-      };
-  
-        await contract.methods.approve(spender,amount).send(tx).then(function (tx) {
+            from: account,
+            to: USDC_TOKEN_ADRRESS,
+            data: encodedABI
+        };
+
+        await web3.eth.sendTransaction(tx).then(function (tx) {
             console.log("Transaction: ", tx);
             //Modify here to add information to the user
         });
-  
+
+
+
+
     } catch (error) {
         // user rejected permission
         console.log(error);
-  
+
     }
-  
-  
-  }
-  
-  
+
+
+}
+
+
 
 
 async function OrderBook_AuthorizeOperator() {
 
     try {
 
-        var contract = new web3.eth.Contract(PACHACUY_TOKEN_ABI, PACHACUY_TOKEN_ADRRESS);
-       var operator = ORDER_BOOK_ADDRESS;
 
+        var operator = ORDER_BOOK_ADDRESS;
+
+        var contract = new web3.eth.Contract(PACHACUY_TOKEN_ABI, PACHACUY_TOKEN_ADRRESS);
+        var txInstancia = contract.methods.authorizeOperator(operator); /*input: methodo  */
+        var encodedABI = txInstancia.encodeABI();
         var tx = {
             from: account,
-            gasLimit: 10000000
+            to: PACHACUY_TOKEN_ADRRESS,
+            data: encodedABI
         };
 
-        await contract.methods.authorizeOperator(operator).send(tx).then(function (tx) {
+        await web3.eth.sendTransaction(tx).then(function (tx) {
             console.log("Transaction: ", tx);
             //Modify here to add information to the user
         });
@@ -64,17 +75,23 @@ async function OrderBook_SetOrder() {
 
     try {
 
+
+        var amountPCUY = $("#amountPCUY").val(); //6 decimals
+        var amountUSDC = $("#amountUSDC").val(); //6 decimals
+        //Obtiene los valores input sin decimales
+
         var contract = new web3.eth.Contract(ORDER_BOOK_ABI, ORDER_BOOK_ADDRESS);
-        var amountPCUY= $("#amountPCUY").val(); //6 decimals
-        var amountUSDC= $("#amountUSDC").val(); //6 decimals
 
 
+        txInstancia = contract.methods.setSellOrder(amountPCUY, amountUSDC); /*input: methodo  */
+        encodedABI = txInstancia.encodeABI();
         var tx = {
             from: account,
-            gasLimit: 10000000
+            to: ORDER_BOOK_ADDRESS,
+            data: encodedABI
         };
 
-        await contract.methods.setSellOrder(amountPCUY,amountUSDC).send(tx).then(function (tx) {
+        await web3.eth.sendTransaction(tx).then(function (tx) {
             console.log("Transaction: ", tx);
             //Modify here to add information to the user
         });
@@ -95,19 +112,26 @@ async function OrderBook_RemoveOrder() {
 
     try {
 
-        var contract = new web3.eth.Contract(ORDER_BOOK_ABI, ORDER_BOOK_ADDRESS);
-        var IDORDER= $("#IDORDER2").val(); //6 decimals
-      
 
+        var IDORDER = $("#IDORDER2").val(); //6 decimals
+
+
+        var contract = new web3.eth.Contract(ORDER_BOOK_ABI, ORDER_BOOK_ADDRESS);
+        var txInstancia = contract.methods.removeSellorder(IDORDER); /*input: methodo  */
+        var encodedABI = txInstancia.encodeABI();
         var tx = {
             from: account,
-            gasLimit: 10000000
+            to: ORDER_BOOK_ADDRESS,
+            data: encodedABI
         };
 
-        await contract.methods.removeSellorder(IDORDER).send(tx).then(function (tx) {
+        await web3.eth.sendTransaction(tx).then(function (tx) {
             console.log("Transaction: ", tx);
             //Modify here to add information to the user
         });
+
+
+
 
 
     } catch (error) {
@@ -125,19 +149,28 @@ async function OrderBook_BuyOrder() {
 
     try {
 
-        var contract = new web3.eth.Contract(ORDER_BOOK_ABI, ORDER_BOOK_ADDRESS);
-        var IDORDER= $("#IDORDER1").val(); //6 decimals
-      
 
+        var IDORDER = $("#IDORDER1").val(); //6 decimals
+
+
+
+        var contract = new web3.eth.Contract(ORDER_BOOK_ABI, ORDER_BOOK_ADDRESS);
+        var txInstancia = contract.methods.buyOrder(IDORDER); /*input: methodo  */
+        var encodedABI = txInstancia.encodeABI();
         var tx = {
             from: account,
-            gasLimit: 10000000
+            to: ORDER_BOOK_ADDRESS,
+            data: encodedABI
         };
 
-        await contract.methods.buyOrder(IDORDER).send(tx).then(function (tx) {
+        await web3.eth.sendTransaction(tx).then(function (tx) {
             console.log("Transaction: ", tx);
             //Modify here to add information to the user
         });
+
+
+
+
 
 
     } catch (error) {
@@ -162,8 +195,8 @@ async function OrderBook_ListOrderPending() {
     try {
 
         var contract = new web3.eth.Contract(ORDER_BOOK_ABI, ORDER_BOOK_ADDRESS);
-        var amountPCUY= $("#amountPCUY").val(); //6 decimals
-        var amountUSDC= $("#amountUSDC").val(); //6 decimals
+        var amountPCUY = $("#amountPCUY").val(); //6 decimals
+        var amountUSDC = $("#amountUSDC").val(); //6 decimals
 
         await contract.methods.listSalesOrderPending().call().then(function (arrays_result) {
             console.log("RESULTS ARRAYS");
@@ -172,11 +205,11 @@ async function OrderBook_ListOrderPending() {
             console.log(arrays_result._usdcAmounts);
             console.log(arrays_result._sellers);
 
-          
-           
-          });
-  
-          
+
+
+        });
+
+
 
     } catch (error) {
         // user rejected permission
